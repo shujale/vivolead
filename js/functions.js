4968a212cecfb801aeafe88b151f7f3fa0a0837f
shujale <hujale.sharmarke@gmail.com>
2019-05-21 11:25:54 +0200

    New update
    Changes on navigation + CSS and JS for hover effect on nav

--- js/functions.js
+++ js/functions.js
@@ -4,10 +4,26 @@ JAVASCRIPT FOR STICKY NAVIGATION AFTER SCROLL
 
 *************************************************/
 
-(new IntersectionObserver(function(e,o){
-    if (e[0].intersectionRatio > 0){
-        document.documentElement.removeAttribute('class');
-    } else {
-        document.documentElement.setAttribute('class','stuck');
-    };
+(new IntersectionObserver(function(e, o) {
+  if (e[0].intersectionRatio > 0) {
+    document.documentElement.removeAttribute('class');
+  } else {
+    document.documentElement.setAttribute('class', 'stuck');
+  };
 })).observe(document.querySelector('.trigger'));
+
+
+/************************************************
+
+JAVASCRIPT FOR DROPDOWN ON HOVER
+
+*************************************************/
+
+(document).ready(function() {
+  $(".dropdown, .btn-group").hover(function() {
+    var dropdownMenu = $(this).children(".dropdown-menu");
+    if (dropdownMenu.is(":visible")) {
+      dropdownMenu.parent().toggleClass("open");
+    }
+  });
+});
